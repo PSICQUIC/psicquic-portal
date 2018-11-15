83be8629d5a7e2b0360beff1fb897b7af90935b9
elisabetb <barrera@ebi.ac.uk>
2018-11-15 15:15:36 +0100

    First final version for the advanced search

--- src/app/search/advanced-search/advanced-search.component.ts
+++ src/app/search/advanced-search/advanced-search.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import {QueryBuilderClassNames, QueryBuilderConfig} from 'angular2-query-builder';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { QueryBuilderConfig} from 'angular2-query-builder';
+import { FormControl } from '@angular/forms';
+
+declare const $: any;
 
 
 @Component({
@@ -9,46 +11,19 @@ import { FormBuilder, FormControl } from '@angular/forms';
   styleUrls: ['./advanced-search.component.css']
 })
 export class AdvancedSearchComponent implements OnInit {
-  public queryCtrl: FormControl;
-  public currentConfig: QueryBuilderConfig;
 
   constructor() {
-    // this.queryCtrl = this.formBuilder.control(this.query);
     this.queryCtrl = new FormControl(this.query);
     this.currentConfig = this.config;
   }
+  public queryCtrl: FormControl;
+  public currentConfig: QueryBuilderConfig;
 
   public query = {
     condition: 'and',
     rules: [
       {field: 'idA', operator: '=', entity: 'participantA'},
     ]
-  };
-
-  public bootstrapClassNames: QueryBuilderClassNames = {
-    removeIcon: 'fa fa-minus',
-    addIcon: 'fa fa-plus',
-    button: 'btn',
-    buttonGroup: 'btn-group',
-    rightAlign: 'order-12 ml-auto',
-    switchRow: 'd-flex px-2',
-    switchGroup: 'd-flex align-items-center',
-    switchRadio: 'custom-control-input',
-    switchLabel: 'custom-control-label',
-    switchControl: 'custom-control custom-radio custom-control-inline',
-    row: 'row p-2 m-1',
-    rule: 'border',
-    ruleSet: 'border',
-    invalidRuleSet: 'alert alert-danger',
-    emptyWarning: 'text-danger mx-auto',
-    operatorControl: 'form-control',
-    operatorControlSize: 'col-auto pr-0',
-    fieldControl: 'form-control',
-    fieldControlSize: 'col-auto pr-0',
-    entityControl: 'form-control',
-    entityControlSize: 'col-auto pr-0',
-    inputControl: 'form-control',
-    inputControlSize: 'col-auto'
   };
 
   public config: QueryBuilderConfig = {
